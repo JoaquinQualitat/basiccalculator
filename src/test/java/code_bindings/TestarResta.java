72c24fb2bd202b04f96576cf6f0e6bdf3c0f1471
Joaquin LOPEZ ORTEGA <joaquin.lopez@soprasteria.com>
2020-11-09 17:30:34 +0100

    Añadido comentario

--- src/test/java/code_bindings/TestarResta.java
+++ src/test/java/code_bindings/TestarResta.java
@@ -19,6 +19,7 @@ public class TestarResta {
 	    // Write code here that turns the phrase above into concrete actions
 	    assertTrue(true);
 	}
+	//Comentario
 
 	@When("^Introduir First number$")
 	public void Introduir_First_number() throws Throwable {
