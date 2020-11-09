25b2cd4be8ae1655a7f1e41db7c6971141a9202b
Joaquin LOPEZ ORTEGA <joaquin.lopez@soprasteria.com>
2020-11-09 12:18:24 +0100

    Commit inicial

---
+++ src/test/java/runners/MyRunner.java
@@ -0,0 +1,23 @@
+package runners;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+//import cucumber.api.junit.Cucumber;
+import com.hpe.alm.octane.OctaneCucumber;
+
+@RunWith (OctaneCucumber.class)
+@CucumberOptions(
+		plugin = {"pretty",
+				"junit:reports/TestResult.xml",
+				"html:reports/cucumber-html-report",
+				"json:reports/cucumber.json",
+				},
+		//tags = {"@functional"},
+		features = {"src/test/java/features"},
+		glue = {"code_bindings"}
+		)
+
+public class MyRunner {
+
+}
