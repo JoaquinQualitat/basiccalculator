24ec3b727458445f9c0f642603cb0628ea9ef068
Joaquin LOPEZ ORTEGA <joaquin.lopez@soprasteria.com>
2020-11-10 10:42:08 +0100

    Quitado entero

--- src/test/java/code_bindings/TestarResta.java
+++ src/test/java/code_bindings/TestarResta.java
@@ -50,8 +50,6 @@ public class TestarResta {
 	public void En_Answer_el_resultat() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 		 assertTrue(false);
-		 int a =22;
-		
 	} 
 	
 	
